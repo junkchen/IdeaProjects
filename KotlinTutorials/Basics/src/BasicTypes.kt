8a67194f690abe563816dba901fec4349ec23b05
Junk Chen <junkchen@aliyun.com>
2018-11-06 16:12:42 +0800

    update

--- KotlinTutorials/Basics/src/BasicTypes.kt
+++ KotlinTutorials/Basics/src/BasicTypes.kt
@@ -1,3 +1,7 @@
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
  * Basic Types
  *
@@ -18,5 +22,35 @@
  * String
  */
 fun main(args: Array<String>) {
+    // unit(s)
+    val time = 4800
+    val hours = time / 3600
+    val minutes = time % 3600 / 60
+    println("hours: $hours, minutes: $minutes")
 
+    println(String.format("%.1f", 0112.3456))
+
+    println(dateConvertMinutes("02:30", "HH:mm"))
+}
+
+/**
+ * 将当日的时间转换成分钟数
+ *
+ * @param date    当日时间
+ * @param pattern 时间格式
+ * @return 分钟数
+ */
+private fun dateConvertMinutes(date: String, pattern: String): Int {
+    val srcSdf = SimpleDateFormat(pattern)
+    try {
+        val d = srcSdf.parse(date)
+        val c = Calendar.getInstance()
+        c.time = d
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+        return hour * 60 + minute
+    } catch (e: ParseException) {
+        e.printStackTrace()
+    }
+    return 0
 }
